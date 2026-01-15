f13c0fdbb4a94a9e6d2728139de071ecfab656ed
cloud4ttlcvs-spec <cloud4ttlcvs@gmail.com>
2026-01-15 16:24:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* sw.js - Service Worker 版本 2 */
-const CACHE_NAME = 'ttl-pwa-v2'; // 修改這裡的版本號來強制更新
+/* sw.js - Service Worker 版本 3 */
+const CACHE_NAME = 'ttl-pwa-v3'; // 修改這裡的版本號來強制更新
 const urlsToCache = [
   './',
   './index.html',
