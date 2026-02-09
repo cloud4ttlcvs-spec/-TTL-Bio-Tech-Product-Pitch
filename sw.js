c76905e8df7f638508ee8825511dc7a7c924c175
cloud4ttlcvs-spec <cloud4ttlcvs@gmail.com>
2026-02-10 00:05:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* sw.js - 強制更新版 */
-const CACHE_NAME = 'ttl-pwa-v25'; // 升級版本號
+const CACHE_NAME = 'ttl-pwa-v26'; // 升級版本號
 const urlsToCache = [
   './',
   './index.html',
@@ -54,5 +54,6 @@ self.addEventListener('message', (event) => {
   }
 
 });
+
 
 
