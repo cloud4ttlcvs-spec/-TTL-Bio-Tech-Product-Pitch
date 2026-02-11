ebf1b30225bca6f3f04d7a2ec4faeecd92eb2ed4
cloud4ttlcvs-spec <cloud4ttlcvs@gmail.com>
2026-02-11 09:36:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - V3.6.1 圖片快取增強版
-const CACHE_NAME = 'ttl-pwa-v39'; // App 核心快取
+const CACHE_NAME = 'ttl-pwa-v40'; // App 核心快取
 const IMAGE_CACHE_NAME = 'ttl-images-v1'; // 專門存圖片的快取
 const urlsToCache = [
   './',
@@ -82,3 +82,4 @@ self.addEventListener('message', (event) => {
   }
 
 });
+
